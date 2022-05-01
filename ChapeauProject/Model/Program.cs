f93b297e901c5eb49de70096c6c91e36cf035cab
mouradjwz <70758569+mouradjwz@users.noreply.github.com>
2022-05-01 23:58:48 +0200

    files

---
+++ ChapeauProject/Model/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Model
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
