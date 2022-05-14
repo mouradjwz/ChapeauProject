a939c9700aadae1117100163f7eb0b7b73b0ad8c
mouradjwz <664995@student.inholland.nl>
2022-05-15 01:23:21 +0200

    added staff to the miodel

---
+++ Model/Staff.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public  class Staff
+    {
+        public string firstName;
+        public string lastname;
+        public int staffId;
+        public int passCode;
+
+        //variables or properties?
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StaffID { get; set; }
+
+        public Staff(string firstName, string lastName, int staffId, int passCode)
+        {
+            this.firstName = firstName;
+            this.lastname = lastName;
+            this.staffId = staffId;
+            this.passCode = passCode;
+        }
+    }
+}
