7ba18327a8f89a5d1184b4d24b5d5ada43215a8c
RobyParak <677676@student.inholland.nl>
2022-05-17 18:57:29 +0200

    Class bill fields created

--- Model/Bill.cs
+++ Model/Bill.cs
@@ -2,4 +2,10 @@
 
 public class Bill
 {
+    public int Id { get; set; }
+    public double AmountDue { get; set; }
+    public double Tip { get; set; }
+    public double TotalDue { get; set; }
+    public string Feedback { get; set; }
+
 }
