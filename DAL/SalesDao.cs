234af542bfa98fe8699ed5c02f78ce7cdd84a500
RobyParak <677676@student.inholland.nl>
2022-05-18 20:14:43 +0200

    GetBill

--- DAL/SalesDao.cs
+++ DAL/SalesDao.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 using Model;
 
@@ -7,13 +9,28 @@ namespace DAL
 {
     public class SalesDao : BaseDao
     {
-        public List<Order> GetOrders()
+        public Bill GetBill(Table table)
         {
-            List<Order> orders = new List<Order>();
-            string query = "SELECT Item.Price as [Price], FROM Item, JOIN [Order_Items] ON Order_ID = Order_ID," +
+            string query = "SELECT SUM(Item.Price) as Price, FROM Item, JOIN [Order_Items] ON Order_ID = Order_ID," +
                 "JOIN [Order] ON Order_ID = Order_ID" +
                 "Where Table_IS is @TableID ";
-        return orders;
+            SqlParameter[] sqlParameters = { new SqlParameter("@TableID", table.Id), };
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
         }
+
+        private Bill ReadTables(DataTable dataTable)
+        {
+            Bill bill = new Bill();
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                {
+                    bill.BillId = (int)dr["ID"],
+                    bill.AmountDue = (int)dr["Price"],
+                };
+
+            }
+            return bill;
+        }
+
+
     }
-}
