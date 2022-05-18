ecc5c34d9255f22b57dc83d056dece88070bd136
mouradjwz <664995@student.inholland.nl>
2022-05-18 13:00:48 +0200

    DAO

---
+++ DAL/StaffDao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+
+namespace DAL
+{
+    public class StaffDao : BaseDao
+    {
+
+
+        public Staff LoginStaff(string firstName, int PassCode)
+        {
+
+        }
+    }
+}
--- Model/Staff.cs
+++ Model/Staff.cs
@@ -6,23 +6,18 @@ namespace Model
 {
     public  class Staff
     {
-        public string firstName;
-        public string lastname;
-        public int staffId;
-        public int passCode;
-
-        //variables or properties?
         
-        //public string FirstName { get; set; }
-        //public string LastName { get; set; }
-        //public string StaffID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int StaffID { get; set; }
+        public int PassCode { get; set; }
 
         public Staff(string firstName, string lastName, int staffId, int passCode)
         {
-            this.firstName = firstName;
-            this.lastname = lastName;
-            this.staffId = staffId;
-            this.passCode = passCode;
+            FirstName = firstName;
+            LastName = lastName;
+            StaffID = staffId;
+            PassCode = passCode;
         }
     }
 }
