240dc8512a62bda7082b10ac9751314874a767b0
mouradjwz <664995@student.inholland.nl>
2022-05-19 14:01:19 +0200

    login

--- Model/Staff.cs
+++ Model/Staff.cs
@@ -5,19 +5,20 @@ using System.Text;
 namespace Model
 {
     public  class Staff
-    {
-        
+    {    
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int StaffID { get; set; }
         public int PassCode { get; set; }
+        public int HasPermission { get; set; }
 
-        public Staff(string firstName, string lastName, int staffId, int passCode)
+        public Staff(string firstName, string lastName, int staffId, int passCode, int hasPermission)
         {
             FirstName = firstName;
             LastName = lastName;
             StaffID = staffId;
             PassCode = passCode;
+            HasPermission = hasPermission;
         }
     }
 }
